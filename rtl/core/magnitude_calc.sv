ccb8b64f8f8d74d22dbffcc2f1f484ea8bc5edcd
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-20 23:14:49 +1100

    Use shift registers for pipelining flags, split up calculation

--- rtl/core/magnitude_calc.sv
+++ rtl/core/magnitude_calc.sv
@@ -14,32 +14,79 @@ module magnitude_calc (
     output logic mag_eop                    // Aligned with frequency bin 1023
     );
 
-    /* Use alpha max + beta min algorithm to approximate magnitude
-    Magnitude is approx. alpha*max(|Re|, |Im|) + beta*min(|Re|, |Im|)
-    Use alpha = 1, beta = 1/4 which means shifting min(|Re|, |Im|) to the right by 2 bits
-    */
+    logic [15:0] abs_real;                  // Absolute value of the real part of the frequency amplitude
+    logic [15:0] abs_imag;                  // Absolute value of the imaginary part of the frequency amplitude
 
-    typedef enum logic {
-        IDLE,
-        FINDING_MAX,
-        FINDING_MIN,
-        APPROXIMATING
-    } calculation_state;
+    logic [15:0] max_val;                   // The maximum value between abs_real and abs_imag
+    logic [15:0] min_val;                   // The minimum value between abs_real and abs_imag
 
-    calculation_state state;
+    // Delay registers for source_valid signal
+    logic valid_delay_1;
+    logic valid_delay_2;
+
+    // Delay registers for source_sop signal
+    logic sop_delay_1;
+    logic sop_delay_2;
+
+    // Delay registers for source_eop signal
+    logic eop_delay_1;
+    logic eop_delay_2;
 
     always_ff @ (posedge board_clk or negedge resetn) begin
         if (!resetn) begin
-            state <= IDLE;
             magnitude <= 16'b0;
             mag_valid <= 1'b0;
             mag_sop <= 1'b0;
             mag_eop <= 1'b0;
+            valid_delay_1 <= 1'b0;
+            valid_delay_2 <= 1'b0;
+            sop_delay_1 <= 1'b0;
+            sop_delay_2 <= 1'b0;
+            eop_delay_1 <= 1'b0;
+            eop_delay_2 <= 1'b0;
         end else begin
-            if (state == IDLE) begin
-                // When in the idle state, check to see if we receive a valid output from FFT
+            // Shift register (3 clock cycles) for mag_valid signal
+            valid_delay_1 <= source_valid;
+            valid_delay_2 <= valid_delay_1;
+            mag_valid <= valid_delay_2;
 
+            // Shift register (3 clock cycles) for mag_sop signal
+            sop_delay_1 <= source_sop;
+            sop_delay_2 <= sop_delay_1;
+            mag_sop <= sop_delay_2;
+
+            // Shift register (3 clock cycles) for mag_eop signal
+            eop_delay_1 <= source_eop;
+            eop_delay_2 <= eop_delay_1;
+            mag_eop <= eop_delay_2;
+
+            // Check if real part of source is negative
+            if (source_real[15])
+                abs_real <= (~source_real) + 16'b1;
+            else
+                abs_real <= source_real;
+
+            // Check if imaginary part of source is negative
+            if (source_imag[15])
+                abs_imag <= (~source_imag) + 16'b1;
+            else
+                abs_imag <= source_imag;
+
+            // Find the maximum/minimum between abs_real and abs_imag
+            if (abs_real > abs_imag) begin
+                max_val <= abs_real;
+                min_val <= abs_imag;
+            end else begin
+                max_val <= abs_imag;
+                min_val <= abs_real;
             end
+
+            /* Using alpha max + beta min algorithm to approximate magnitude
+            Magnitude is approx. alpha*max(|Re|, |Im|) + beta*min(|Re|, |Im|)
+            Using alpha = 1, beta = 1/4 which means shifting min(|Re|, |Im|) to the right by 2 bits
+            */
+
+            magnitude <= max_val + (min_val >> 2'd2);
         end
     end
 
