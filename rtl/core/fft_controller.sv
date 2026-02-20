d92f07d1a618bc28238124713d37ba4033baa4eb
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-20 21:43:03 +1100

    First draft for fft input logic

--- rtl/core/fft_controller.sv
+++ rtl/core/fft_controller.sv
@@ -21,6 +21,54 @@ module fft_controller {
     output logic sink_eop               // Pulses at the end of an audio packet
     };
 
+    logic [9:0] word_counter = 0;       // Counts how many words we have sent to the FFT module
+
+    always_ff @ (posedge board_clk or negedge resetn) begin
+        if (!resetn) begin
+            rdreq <= 1'b0;
+            sink_real <= 16'b0;
+            sink_valid <= 1'b0;
+            sink_sop <= 1'b0;
+            sink_eop <= 1'b0;
+            word_counter <= 1'b0;
+
+        // If the fifo isn't empty
+        end else (!rdempty) begin
+            // Default to having sink pulses as low
+            sink_sop <= 1'b0;
+            sink_eop <= 1'b0;
+
+            // Check if we have enough words (1024) to send to the FFT
+            if (rdusedw >= 12'd1024) begin
+                rdreq <= 1'b1;          // Request a read operation
+
+                // If the FFT is ready to read our data
+                if (sink_ready) begin
+                    sink_valid <= 1'b1;     // Indicate sink is valid
+                    sink_real <= fifo_q;    // Send fifo output to FFT
+                end
+
+                // Indicate start of packet if this is the first word we are sending
+                if (word_counter == 10'b0;)
+                    sink_sop <= 1'b1;
+
+                // Indicate end of packet if this is the last word we are sending  
+                else if (word_counter == 10'd1023)
+                    sink_eop <= 1'b1;
+
+                if (word_counter < 1023)
+                    word_counter <= word_counter + 10'd1;
+                else
+                    word_counter <= 10'b0;
+            end
+
+
+        // If the fifo is empty
+        end else begin
+            // Don't read from it
+            rdreq <= 1'b0;
+        end
+    end
 
 
 endmodule
