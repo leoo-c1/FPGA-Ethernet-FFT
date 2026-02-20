1bb74c7be5dd3a7b26c320b0e86a96c46712de88
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-20 13:40:44 +1100

    Add altera fifo ip core files

---
+++ rtl/fifo/audio_fifo_inst.v
@@ -0,0 +1,11 @@
+audio_fifo	audio_fifo_inst (
+	.data ( data_sig ),
+	.rdclk ( rdclk_sig ),
+	.rdreq ( rdreq_sig ),
+	.wrclk ( wrclk_sig ),
+	.wrreq ( wrreq_sig ),
+	.q ( q_sig ),
+	.rdempty ( rdempty_sig ),
+	.rdusedw ( rdusedw_sig ),
+	.wrfull ( wrfull_sig )
+	);
