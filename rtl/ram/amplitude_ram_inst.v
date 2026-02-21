21f3be11a70bf3c77d4ce16bdbcb113c35cf7710
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-21 22:51:59 +1100

    Add dual-port RAM quartus IP core

---
+++ rtl/ram/amplitude_ram_inst.v
@@ -0,0 +1,9 @@
+amplitude_ram	amplitude_ram_inst (
+	.data ( data_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.rdclock ( rdclock_sig ),
+	.wraddress ( wraddress_sig ),
+	.wrclock ( wrclock_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
