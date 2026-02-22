d436a550ddc949fd708d3d77701da88e0b0a7236
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-22 21:46:51 +1100

    Draft reading logic and bar rendering

--- rtl/core/vga_driver.sv
+++ rtl/core/vga_driver.sv
@@ -15,8 +15,35 @@ module vga_driver (
     output logic blue                   // Pixel blue value (single bit, 0v or 0.7v)
     );
 
-    always_ff @ (posedge board_clk or negedge resetn) begin
+    always_ff @ (posedge pll_clk or negedge resetn) begin
         if (!resetn) begin
+            rdaddress <= 10'b0;
+            red <= 1'b0;
+            green <= 1'b0;
+            blue <= 1'b0;
+        end else begin
+            // Reading from the RAM takes one clock cycle, so read for upcoming pixel_x
+            if (pixel_x < 639) begin
+                // Approximate multiplication by 1.6 (approx. 205/128)
+                rdaddress <= (205*(pixel_x + 1'b1)) >> 3'd7;
+            end else if (pixel_x == 10'd799) begin
+                // On the very last pixel, schedule a read from the first memory address
+                rdaddress <= 10'b0;
+            end
+
+            if (video_on) begin
+                if (pixel_y < q_amplitude) begin
+                    // Colour the bar white
+                    red <= 1'b1;
+                    green <= 1'b1;
+                    blue <= 1'b1;
+                end else begin
+                    // If we're above the bar, colour it black
+                    red <= 1'b0;
+                    green <= 1'b0;
+                    blue <= 1'b0;
+                end
+            end
 
         end
     end
