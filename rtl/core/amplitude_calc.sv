12304b79b94f19588e1ee8e6f163aa13b9f3515b
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-22 21:00:51 +1100

    Take the pseudo-logarithm of the amplitude for display sclaing

--- rtl/core/amplitude_calc.sv
+++ rtl/core/amplitude_calc.sv
@@ -9,7 +9,7 @@ module amplitude_calc (
     input logic source_eop,                 // End of packet (frequency bin 1023)
     input logic [5:0] source_exp,           // The exponent of the block-floating-point representation of source
 
-    output logic [30:0] amplitude,          // The calculated frequency amplitude after applying exponent scaling
+    output logic [15:0] amplitude,          // Frequency amplitude (after exponent and logarithmic scaling)
     output logic amp_valid,                 // Pulses high when 'amplitude' is ready
     output logic amp_sop,                   // Aligned with frequency bin 0
     output logic amp_eop                    // Aligned with frequency bin 1023
@@ -22,6 +22,7 @@ module amplitude_calc (
     logic [15:0] min_val;                   // The minimum value between abs_real and abs_imag
 
     logic [30:0] base_amplitude;            // The amplitude before exponent scaling is applied
+    logic [30:0] raw_amplitude;             // The exponent-scaled amplitude before logarithmic scaling
 
     logic [5:0] delayed_exp;                // The delayed exponent of the block-floating-point representation of source
 
@@ -29,16 +30,19 @@ module amplitude_calc (
     logic valid_delay_1;
     logic valid_delay_2;
     logic valid_delay_3;
+    logic valid_delay_4;
 
     // Delay registers for source_sop signal
     logic sop_delay_1;
     logic sop_delay_2;
     logic sop_delay_3;
+    logic sop_delay_4;
 
     // Delay registers for source_eop signal
     logic eop_delay_1;
     logic eop_delay_2;
     logic eop_delay_3;
+    logic eop_delay_4;
 
     // Delay registers for source_exp value
     logic [5:0] exp_delay_1;
@@ -47,7 +51,7 @@ module amplitude_calc (
 
     always_ff @ (posedge board_clk or negedge resetn) begin
         if (!resetn) begin
-            amplitude <= 31'b0;
+            amplitude <= 16'b0;
             base_amplitude <= 31'b0;
             delayed_exp <= 6'b0;
             amp_valid <= 1'b0;
@@ -56,33 +60,39 @@ module amplitude_calc (
             valid_delay_1 <= 1'b0;
             valid_delay_2 <= 1'b0;
             valid_delay_3 <= 1'b0;
+            valid_delay_4 <= 1'b0;
             sop_delay_1 <= 1'b0;
             sop_delay_2 <= 1'b0;
             sop_delay_3 <= 1'b0;
+            sop_delay_4 <= 1'b0;
             eop_delay_1 <= 1'b0;
             eop_delay_2 <= 1'b0;
             eop_delay_3 <= 1'b0;
+            eop_delay_4 <= 1'b0;
             exp_delay_1 <= 1'b0;
             exp_delay_2 <= 1'b0;
             exp_delay_3 <= 1'b0;
         end else begin
-            // Shift register (4 clock cycles) for amp_valid signal
+            // Shift register (5 clock cycles) for amp_valid signal
             valid_delay_1 <= source_valid;
             valid_delay_2 <= valid_delay_1;
             valid_delay_3 <= valid_delay_2;
-            amp_valid <= valid_delay_3;
+            valid_delay_4 <= valid_delay_3;
+            amp_valid <= valid_delay_4;
 
-            // Shift register (4 clock cycles) for amp_sop signal
+            // Shift register (5 clock cycles) for amp_sop signal
             sop_delay_1 <= source_sop;
             sop_delay_2 <= sop_delay_1;
             sop_delay_3 <= sop_delay_2;
-            amp_sop <= sop_delay_3;
+            sop_delay_4 <= sop_delay_3;
+            amp_sop <= sop_delay_4;
 
-            // Shift register (4 clock cycles) for amp_eop signal
+            // Shift register (5 clock cycles) for amp_eop signal
             eop_delay_1 <= source_eop;
             eop_delay_2 <= eop_delay_1;
             eop_delay_3 <= eop_delay_2;
-            amp_eop <= eop_delay_3;
+            eop_delay_4 <= eop_delay_3;
+            amp_eop <= eop_delay_4;
 
             // Shift register (4 clock cycles) for delayed_exp signal
             exp_delay_1 <= source_exp;
@@ -120,11 +130,63 @@ module amplitude_calc (
 
             // Decide the direction of bit shift based on sign of the exponent
             if (delayed_exp[5]) 
-                amplitude <= base_amplitude << (~delayed_exp + 1'b1);
+                raw_amplitude <= base_amplitude << (~delayed_exp + 1'b1);
             
             else
-                amplitude <= base_amplitude >> delayed_exp;
+                raw_amplitude <= base_amplitude >> delayed_exp;
         end
+    end
+
+    logic [31:0] val_32;
+    logic [15:0] val_16;
+    logic [7:0] val_8;
+    logic [3:0] val_4;
+    logic [1:0] val_2;
+
+    always_comb begin
+        val_32 = {1'b0, raw_amplitude}; // Pad to 32 bits
+
+        // Find the 16s place
+        if (| val_32[31:16]) begin
+            amplitude[4] = 1'b1;
+            val_16 = val_32[31:16]; // The 1 is in the top half, so keep the top half
+        end else begin
+            amplitude[4] = 1'b0;
+            val_16 = val_32[15:0];  // The 1 is in the bottom half, so keep the bottom half
+        end
+
+        // Find the 8s place
+        if (| val_16[15:8]) begin
+            amplitude[3] = 1'b1;
+            val_8 = val_16[15:8];
+        end else begin
+            amplitude[3] = 1'b0;
+            val_8 = val_16[7:0];
+        end
+
+        // Find the 4s place
+        if (| val_8[7:4]) begin
+            amplitude[2] = 1'b1;
+            val_4 = val_8[7:4];
+        end else begin
+            amplitude[2] = 1'b0;
+            val_4 = val_8[3:0];
+        end
+
+        // Find the 2s place
+        if (| val_4[3:2]) begin
+            amplitude[1] = 1'b1;
+            val_2 = val_4[3:2];
+        end else begin
+            amplitude[1] = 1'b0;
+            val_2 = val_4[1:0];
+        end
+
+        // Find the 1s place
+        if (val_2[1])
+            amplitude[0] = 1'b1;
+        else
+            amplitude[0] = 1'b0;
     end
 
 endmodule
