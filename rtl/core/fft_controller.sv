78b4bc097f1b9cb678e01003ac3238ae43717fe5
leoo-c1 <chinchillaleo01@gmail.com>
2026-03-01 12:49:04 +1100

    Don't send signals to FFT when flush is requested

--- rtl/core/fft_controller.sv
+++ rtl/core/fft_controller.sv
@@ -1,6 +1,7 @@
 /*
 Handles reading from the FIFO buffer and connecting the read data to the FFT.
-The onboard 50MHz clock is used for reading the FIFO buffer
+The onboard 50MHz clock is used for reading the FIFO buffer.
+Ignores corrupt frames as indicated by the ethernet parser.
 */
 
 module fft_controller (
@@ -11,6 +12,7 @@ module fft_controller (
     input logic [11:0] rdusedw,         // How many words (1 word = 16 bits) are currently in the FIFO
 
     input logic sink_ready,             // Flag from the FFT telling us it is ready to read data
+    input logic payload_flush,          // NEW: Flag from ethernet parser indicating sequence error
 
     output logic rdreq,                 // Requests a read operation when high
 
@@ -30,21 +32,34 @@ module fft_controller (
 
     logic [9:0] word_counter = 0;       // Counts how many words we have sent to the FFT module
     logic [3:0] wait_counter = 0;       // Counts how many clock cycles we have been waiting for the FIFO to catch up
+    
+    logic corrupt_frame_latch;          // Goes high when a sequence error occurs
 
     assign sink_real = fifo_q;
     
-    // sink_valid is high only when we are in the READING state
-    assign sink_valid = (state == READING);
+    // If the frame is corrupt, fft signals stay 0
+    assign sink_valid = (state == READING) && !corrupt_frame_latch;
+    assign sink_sop = (state == READING) && (word_counter == 10'd0) && !corrupt_frame_latch;
+    assign sink_eop = (state == READING) && (word_counter == 10'd1023) && !corrupt_frame_latch;
     
-    // sink_sop is high only on the very first word (count 0)
-    assign sink_sop = (state == READING) && (word_counter == 10'd0);
-    
-    // sink_eop is high only on the very last word (count 1023)
-    assign sink_eop = (state == READING) && (word_counter == 10'd1023);
-    
-    // Read from FIFO only if we are sending valid data and the FFT is ready to take it
+    // Read from FIFO only if we are sending valid data and the FFT is ready to take it.
     assign rdreq = (state == READING) && sink_ready;
     
+    // Handle the corrupt frame latch
+    always_ff @ (posedge board_clk or negedge resetn) begin
+        if (!resetn) begin
+            corrupt_frame_latch <= 1'b0;
+        end else begin
+            if (payload_flush) begin
+                // Sequence error occurred, latch the error high
+                corrupt_frame_latch <= 1'b1;
+            end else if (state == READING && word_counter == 10'd1023 && sink_ready) begin
+                // Clear the latch to get ready for the next frame
+                corrupt_frame_latch <= 1'b0;
+            end
+        end
+    end
+
     // State machine and FIFO read request
     always_ff @ (posedge board_clk or negedge resetn) begin
         if (!resetn) begin
