a7f75d306ca6e07028ca95e6520f574d486d8635
leoo-c1 <chinchillaleo01@gmail.com>
2026-03-02 23:28:46 +1100

    Copy over working eth_parser from Ethernet project

--- rtl/eth/eth_parser.sv
+++ rtl/eth/eth_parser.sv
@@ -5,7 +5,7 @@ module eth_parser #(
     parameter FPGA_IP = 32'hC0_00_02_92,        // FPGA's IP address of 192.0.2.146 (theoretical)
     parameter FPGA_PORT = 16'd5005              // FPGA's UDP port (theoretical)
     ) (
-    input logic board_clk,                // 50MHz LAN8720 clock
+    input logic clk,                    // 50MHz LAN8720 clock
     input logic resetn,                 // Reset button (active low)
 
     input logic data_valid,             // Flag to indicate we are receiving valid data
@@ -16,25 +16,45 @@ module eth_parser #(
     output logic [7:0] payload,         // The payload data
     output logic payload_valid,         // Whether we are currently receiving payload data
     output logic payload_last,          // Pulses on the last byte of our payload data
-    output logic payload_flush          // Tells the FIFO to drop the half-filled frame
+    output logic payload_flush          // NEW: Tells the FIFO to drop the frame on sequence error
     );
 
     frame_header frame_header_content;  // Each component of the ethernet frame header
     ip_header ip_header_content;        // Each component of the IP packet header
     udp_header udp_header_content;      // Each component of the UDP datagram header
 
-    eth_states state;                   // The current ethernet state we are in
+    (* keep *) eth_states state;        // The current ethernet state we are in
+
+    logic [47:0] dest_mac_flat;
+    logic [15:0] ethertype_flat;
+    logic [31:0] dest_ip_flat;
+    logic [15:0] dest_port_flat;
+    logic [15:0] udp_len_flat;
+
+    // Destination MAC
+    assign dest_mac_flat = {frame_header_content.dest_mac[0], frame_header_content.dest_mac[1],
+                            frame_header_content.dest_mac[2], frame_header_content.dest_mac[3],
+                            frame_header_content.dest_mac[4], frame_header_content.dest_mac[5]};
 
     // Ethertype, low byte is the current received byte at the time of checking this
-    logic [15:0] ethertype_flat;
     assign ethertype_flat = {frame_header_content.ethertype[0], received_byte};
+
+    // Destination IP address
+    assign dest_ip_flat = {ip_header_content.dest_ip[0], ip_header_content.dest_ip[1],
+                           ip_header_content.dest_ip[2], ip_header_content.dest_ip[3]};
+
+    // Destination UDP port
+    assign dest_port_flat = {udp_header_content.dest_port[0], udp_header_content.dest_port[1]};
+
+    // UDP length
+    assign udp_len_flat = {udp_header_content.udp_len[0], udp_header_content.udp_len[1]};
 
     logic [16:0] ip_checksum_calc;      // The calculated checksum of the IP header
     logic [31:0] ip_checksum_acc;       // 32 bits to handle overflow carries
     logic [15:0] current_word;          // Temporary holder for the 16-bit word
-    
+
     logic [7:0] expected_seq;           // Tracks expected sequence ID (0x01 or 0x02)
-    logic ignore_payload;               // Indicates whether to drop incoming packet
+    logic ignore_payload;               // Flags whether to drop incoming packet
 
     always_comb begin
         // Add any carry-over to the bottom 4 hex digits of the sum
@@ -48,7 +68,7 @@ module eth_parser #(
 
     logic [15:0] byte_counter = 0;      // Counts the number of bytes we have received
 
-    always_ff @ (posedge board_clk or negedge resetn) begin
+    always_ff @ (posedge clk or negedge resetn) begin
         if (!resetn) begin
             state <= IDLE;
             payload <= 8'b0;
@@ -59,8 +79,15 @@ module eth_parser #(
             ip_checksum_acc <= 0;
             expected_seq <= 8'h01;
             ignore_payload <= 1'b0;
-        end else if (data_valid) begin
-            // If data_valid is high we are receiving an ethernet frame, progress through the states
+        end else if (!data_valid) begin
+            // Force idle if we aren't receiving valid data
+            state <= IDLE;
+            payload_valid <= 1'b0;
+            payload_last <= 1'b0;
+            payload_flush <= 1'b0;
+            byte_counter <= 0;
+            ip_checksum_acc <= 0;
+        end else begin
             if (state == IDLE) begin
                 payload_valid <= 1'b0;
                 payload_last <= 1'b0;
@@ -90,10 +117,10 @@ module eth_parser #(
                         byte_counter <= 0;
                         ip_checksum_acc <= 0;
                         // Make sure the destination mac is ours and ethertype is 0x0800 (IPv4)
-                        if ((frame_header_content.dest_mac == FPGA_MAC) & (ethertype_flat == 16'h0800))
+                        if ((dest_mac_flat == FPGA_MAC) & (ethertype_flat == 16'h0800))
                             state <= IP_HEADER;
                         else
-                            state <= IDLE;
+                            state <= DROP_FRAME;
                     end
                 end
 
@@ -123,9 +150,9 @@ module eth_parser #(
                         end
                         7: ip_header_content.frag_offset[7:0] <= received_byte;
 
-                        8: ip_header_content.ttl <= received_byte;
+                        8: ip_header_content.ttl[0] <= received_byte;
 
-                        9: ip_header_content.protocol <= received_byte;
+                        9: ip_header_content.protocol[0] <= received_byte;
 
                         10: ip_header_content.header_csum[0] <= received_byte;
                         11: ip_header_content.header_csum[1] <= received_byte;
@@ -166,8 +193,8 @@ module eth_parser #(
                 if (byte_valid) begin
                     if (byte_counter == 0) begin
                         // Check if checksum is the valid 0x0000 and dest_ip matches the FPGA's IP
-                        if (~(ip_checksum_calc == 16'h0000) | ~(ip_header_content.dest_ip == FPGA_IP))
-                            state <= IDLE;
+                        if (~(ip_checksum_calc == 16'h0000) | ~(dest_ip_flat == FPGA_IP))
+                            state <= DROP_FRAME;
                     end
 
                     // Assign bytes based on the current byte counter
@@ -192,42 +219,38 @@ module eth_parser #(
                         current_word <= 16'b0;
 
                         // Check if dest_port matches the FPGA's port
-                        if (udp_header_content.dest_port == FPGA_PORT)
+                        if (dest_port_flat == FPGA_PORT)
                             state <= PAYLOAD;
                         else
-                            state <= IDLE;
+                            state <= DROP_FRAME;
                     end
                 end
 
             end else if (state == PAYLOAD) begin
-                payload_valid <= 1'b1;
                 if (byte_valid) begin
                     if (byte_counter == 0) begin
-                        // Intercept the sequence ID byte
+                        // INTERCEPT SEQUENCE ID: Do not assert payload_valid
                         payload_valid <= 1'b0;
                         if (received_byte == 8'h01) begin
-                            // If we missed a 0x02, flush the first half
                             if (expected_seq == 8'h02) begin
-                                payload_flush <= 1'b1;
+                                payload_flush <= 1'b1; // Missed a 0x02, flush the old 0x01 data
                             end
                             expected_seq <= 8'h02;
                             ignore_payload <= 1'b0;
                         end else if (received_byte == 8'h02) begin
                             if (expected_seq == 8'h02) begin
-                                expected_seq <= 8'h01;
+                                expected_seq <= 8'h01; // Perfect sequence
                                 ignore_payload <= 1'b0;
-                            // If we missed a 0x01, flush this half
                             end else begin
-                                ignore_payload <= 1'b1;
+                                ignore_payload <= 1'b1; // Missed a 0x01, drop this 0x02
                             end
-                        // If we got an invalid sequence ID, flush this half
                         end else begin
-                            ignore_payload <= 1'b1;
+                            ignore_payload <= 1'b1; // Invalid sequence ID
                         end
-                        byte_counter <= byte_counter + 1'b1;    // Move to actual payload data
+                        byte_counter <= byte_counter + 1'b1; // Move to actual payload data
                     end else begin
-                        // Actual payload data
-                        payload_flush <= 1'b0;      // Turn off flush flag
+                        // ACTUAL AUDIO DATA: Send to FIFO
+                        payload_flush <= 1'b0; // Deassert flush flag
                         if (!ignore_payload) begin
                             payload <= received_byte;
                             payload_valid <= 1'b1;
@@ -235,38 +258,40 @@ module eth_parser #(
                             payload_valid <= 1'b0;
                         end
 
-                        if (byte_counter < udp_header_content.udp_len - 16'd9) begin
+                        if (byte_counter < udp_len_flat - 16'd9) begin
                             byte_counter <= byte_counter + 1'b1;
                             payload_last <= 1'b0;
-                        end else if (byte_counter == udp_header_content.udp_len - 16'd9) begin
+                        end else if (byte_counter == udp_len_flat - 16'd9) begin
                             byte_counter <= 0;
                             payload_last <= (!ignore_payload) ? 1'b1 : 1'b0;
                             state <= FCS;
                         end
                     end
+                end else begin
+                    payload_valid <= 1'b0;
                 end
 
             end else if (state == FCS) begin
-                if (byte_valid) begin
-                    payload_last <= 1'b0;
-                    payload_valid <= 1'b0;
+                // Clear payload_last when we enter FCS
+                payload_last <= 1'b0;
+                payload_valid <= 1'b0;
 
+                if (byte_valid) begin
                     if (byte_counter < 4)
                         byte_counter <= byte_counter + 1'b1;
                     else
                         state <= IDLE;
                 end
+            
+            end else if (state == DROP_FRAME) begin
+                // Ignore frame
+                payload_valid <= 1'b0;
+                payload_last <= 1'b0;
 
             end else begin
                 state <= IDLE;
-            end
-        end else begin
-            // Go back to idle if data_valid is gone
-            if (!data_valid) begin
-                state <= IDLE;
                 payload_valid <= 1'b0;
                 payload_last <= 1'b0;
-                payload_flush <= 1'b0;
                 byte_counter <= 0;
             end
         end
